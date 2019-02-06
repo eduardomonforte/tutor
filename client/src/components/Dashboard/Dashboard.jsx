c068148e6107009c7b261d10816c264759cca6b4
eduardo moran <eduardo.moran.davalos@gmail.com>
2019-02-06 09:30:25 +0600

    minor dashboard styling

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
-import { Container, Row, Col, CardDeck } from 'react-bootstrap';
+import { Jumbotron, Container, Row, Col, CardDeck } from 'react-bootstrap';
+import Navbar from '../CustomNavbar/CustomNavbar';
+import Footer from '../Footer/Footer';
 import ResourceCard from '../ResourceCard/ResourceCard';
 import resources from '../../resources/resourcesDB.json';
 import sortResources from '../../resources/sortResources';
+import './Dashboard.css';
+
 
 class Dashboard extends Component {
     state = {
@@ -21,22 +25,32 @@ class Dashboard extends Component {
     
     render() {
         return (
-            <Container>
-                <Row>
-                    <Col size="md-3 sm-6">
-                        <CardDeck>
-                            {this.state.resources.map(resource => (
-                                <ResourceCard
-                                    key={resource.id}
-                                    name={resource.name}
-                                    description={resource.description}
-                                    karma={resource.karma}
-                                />
-                            ))} 
-                        </CardDeck>
-                    </Col>
-                </Row>
-            </Container>
+            <Container fluid>
+            <Jumbotron fluid>
+                <Navbar />
+                    <Row className="show-grid text-justify">
+                        <Col xs={6}>
+                        <h1>Hello, janeDoe</h1>
+                        <h2>Now we don't want him to get lonely, so we'll give him a little friend. Brown is such a nice color. We'll paint one happy little tree right here. Decide where your cloud lives. Maybe he lives right in here. I think there's an artist hidden in the bottom of every single one of us. Just let these leaves jump off the brush</h2>
+                        </Col>
+                    </Row>
+            </Jumbotron>
+            <Row>
+                <Col xs={12}>
+                    <CardDeck style={{ display: 'flex', justifyContent:'center', alignItems:'center' }}>
+                        {this.state.resources.map(resource => (
+                            <ResourceCard
+                                key={resource.id}
+                                name={resource.name}
+                                description={resource.description}
+                                karma={resource.karma}
+                            />
+                        ))} 
+                    </CardDeck>
+                </Col>
+            </Row>
+            <Footer />
+        </Container>
         )
     }
 }
