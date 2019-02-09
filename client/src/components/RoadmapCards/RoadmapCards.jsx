c6f1dc3b5e74810565e2ec61d1ad653746101805
Eduardo Monforte <eduardomonforte@gmail.com>
2019-02-09 13:23:19 -0600

    some front end stuff.

--- client/src/components/RoadmapCards/RoadmapCards.jsx
+++ client/src/components/RoadmapCards/RoadmapCards.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './icons';
 
 const RoadmapCards = props => (
-    <div 
-        className="card" 
-        value={props.id} 
+    <div
+        className="card"
+        value={props.id}
         onClick={() => props.handleClick(props.id)}>
         <div className="img-container">
             <img alt={props.name} title={props.name} src={props.image} />
