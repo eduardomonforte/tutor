8e09cbfeee67b2d5b11d46b5b79954ae80fac648
eduardo moran <eduardo.moran.davalos@gmail.com>
2019-02-13 03:12:11 +0600

    responsive dashboard

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -67,7 +67,7 @@ class Home extends Component {
                     ))}
                     </CardDeck>
                 </Row>
-                <Footer />s
+                <Footer />
             </Container >
         );
     }
--- client/src/components/ResourceCard/ResourceCard.jsx
+++ client/src/components/ResourceCard/ResourceCard.jsx
@@ -4,21 +4,27 @@ import "../RoadmapCards/RoadmapCards";
 import './ResourceCard.css';
 import './assets';
 
-const ResourceCard = props => (
-  <Card style={{ align: 'top', width: '18rem' }}>
-    <Card.Img variant="top" src="holder.js/100px160" style={{ height: '180px' }} />
-    <Card.Body>
-      <Card.Title>{props.title}</Card.Title>
-      <Card.Text>{props.description}</Card.Text>
-    </Card.Body>
-    <Card.Footer>
-      <img alt="whatever" src="/assets/tutor-bookmark.png" width="auto" height="30px" />
-      <small className="card-karma">Karma: {props.rating}</small>
-      <span className="button-container" height="30px">
-        <Button variant="danger" className="justify-content-right">-</Button><Button variant="success" className="justify-content-right">+</Button>
-      </span>
-    </Card.Footer>
-  </Card>
-);
-
+function ResourceCard (props) {
+  return (
+    <div>
+        <Card style={{ align: 'top', width: '18rem' }}>
+          <Card.Img variant="top" src="holder.js/100px160" style={{ height: '180px' }} />
+          <Card.Body>
+            <Card.Title>{props.title}</Card.Title>
+            <Card.Text>{props.description}</Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            {/* <Col>
+              <Button
+            </Col> */}
+            // <img className="bookmark-icon" alt="bookmark" src="/assets/tutor-bookmark.png" width="auto" height="40px" />
+            // <small className="card-karma">Karma: {props.rating}</small>
+            // <span className="button-container" height="30px">
+            //   <Button variant="danger" className="justify-content-right">-</Button><Button variant="success" className="justify-content-right">+</Button>
+            </span>
+          </Card.Footer>
+        </Card>
+    </div>
+  );
+}
 export default ResourceCard;
