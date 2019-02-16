008414c94bae69dae1233ec21504aa4337ab1c99
jonabari <barcelo.jonathan@gmail.com>
2019-02-16 02:20:56 -0600

    lkasndf

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,7 @@ var passport = require('passport');
 router.get('/login', passport.authenticate('auth0', {
   scope: 'openid email profile'
 }), function (req, res) {
-  res.redirect('/');
+  res.redirect('/dashboard');
 });
 
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
