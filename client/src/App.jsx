5c63607707cbda62eda3f5b229298a6847bdc799
jonabari <barcelo.jonathan@gmail.com>
2019-02-15 21:29:35 -0600

    bug fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
-import Autho from './components/Autho/Autho';
 import Dashboard from './components/Dashboard/Dashboard';
 import NoMatch from './components/NoMatch/NoMatch';
 import Terms from './components/Terms/Terms';
@@ -13,7 +12,6 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/" component ={Home} />
-          <Route path ="/Autho" component ={Autho} />
           <Route path="/Dashboard" component ={Dashboard} />
           <Route path="/NoMatch" component ={NoMatch} />
           <Route path="/Terms" component={Terms} />
