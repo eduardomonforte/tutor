d4adbee40f70d3b8143cdbd167cde9a1178aef64
Eduardo Monforte <eduardomonforte@gmail.com>
2019-02-16 03:21:09 -0600

    working locally.

--- client/src/components/NoMatch/NoMatch.jsx
+++ client/src/components/NoMatch/NoMatch.jsx
@@ -9,17 +9,17 @@ class NoMatch extends Component {
             <div className="NoMatch">
                 <Container fluid>
                     <Row>
-                        <Col style={{ display: 'flex', justifyContent:'center', alignItems:'center', width: '100%' }}>
-                            <Image src="assets/bookmark-neon.png" className="bookmark-neon" Reactstyle style={{display: 'flex', justifyContent: 'center'}} />
+                        <Col style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%' }}>
+                            <Image src="assets/bookmark-neon.png" className="bookmark-neon" Reactstyle style={{ display: 'flex', justifyContent: 'center' }} />
                         </Col>
                     </Row>
                     <Row>
-                        <Col style={{ display: 'flex', justifyContent:'center', alignItems:'center' }}>
-                            <p className="nomatch-text">...you seem to have reached the backend</p>
+                        <Col style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                            <p className="nomatch-text">...you seem to have reached the back-end.</p>
                         </Col>
                     </Row>
                     <Row>
-                        <Col style={{ display: 'flex', justifyContent:'center', alignItems:'center' }}>
+                        <Col style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                             <Button href="/" variant="secondary" className="nomatch-button" id="nomatch" size="lg" >Take me home!</Button>
                         </Col>
                     </Row>
