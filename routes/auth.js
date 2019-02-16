f246eaeb856f18f6f9af452a962209f733a9afb6
jonabari <barcelo.jonathan@gmail.com>
2019-02-16 02:52:10 -0600

    akmnsf

--- routes/auth.js
+++ routes/auth.js
@@ -10,10 +10,10 @@ router.get('/login', passport.authenticate('auth0', {
 });
 
 // Perform the final stage of authentication and redirect to previously requested URL or '/user'
-router.get('/dashboard', function (req, res, next) {
+router.get('/callback', function (req, res, next) {
   passport.authenticate('auth0', function (err, user, info) {
     if (err) { return next(err); }
-    // if (!user) { return res.redirect('/login'); }
+    if (!user) { return res.redirect('/login'); }
     req.logIn(user, function (err) {
       if (err) { return next(err); }
       const returnTo = req.session.returnTo;
