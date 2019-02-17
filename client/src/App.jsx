793cde3fa23660e043a402b5c71316c1dd4dad1d
Eduardo Monforte <eduardomonforte@gmail.com>
2019-02-16 20:53:41 -0600

    changed the name of the home component.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './components/Home/Home';
+import Home from './components/Home';
 import Dashboard from './components/Dashboard/Dashboard';
 import NoMatch from './components/NoMatch/NoMatch';
 import Terms from './components/Terms/Terms';
